8c9cadf48f489ccb6b7a520edbcddafe7942ed58
Jacopo Costantini <macpapo@Jacopos-MacBook-Pro.local>
2021-04-02 16:02:36 +0200

    ediolognomomongoloide

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <div className="App">Dio Cane</div>;
-}
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
 export default App;
