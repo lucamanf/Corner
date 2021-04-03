cdadd144d899c7a49b1f46bb4b936f7737ca48ad
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-04-03 13:29:14 +0200

    add alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -15,6 +16,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register}></Route>
             <Route exact path="/login" component={Login}></Route>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,12 +20,9 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("🚀 ~ file: Register.js ~ line 21 ~ Register ~ diocane");
+      setAlert("Metti bene la password valà dio cane", "danger");
     } else {
-      console.log(
-        "🚀 ~ file: Register.js ~ line 23 ~ onSubmit ~ Merda DiiiOOOOO",
-        formData
-      );
+      console.log("SUCCESS");
     }
   };
 
@@ -85,4 +85,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+}; 
+
+export default connect(null, { setAlert })(Register);
