925863a7fb25990f962c1d29a5eeac3f2f36ad34
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-04-10 19:35:52 +0200

    piccole modifiche cards

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Card = (props) => (
   <div class="p-4 md:w-1/3 ">
-    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-2xl overflow-hidden">
       <img
-        class="lg:h-48 md:h-36 w-full object-cover object-center"
+        class="lg:h-48 md:h-36 w-full object-cover object-center px-2 py-2 rounded-2xl"
         alt="project"
         src={props.project.image_src}
       />
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const SearchBox = ({ placeholder, handleChange }) => (
   <div class="p-8">
-    <div class="bg-white flex items-center rounded-full shadow-xl">
+    <div class="bg-white flex items-center rounded-full shadow-xl m-20">
       <input
-        class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+        class="rounded-l-full w-full py-0 px-6 text-gray-700 leading-tight focus:outline-none"
         id="search"
         type="text"
         placeholder="Search"
