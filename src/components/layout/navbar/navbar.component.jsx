7ef9e30ba6c260b6a4025bd328043e3ef13a4420
Jacopo Costantini <macpapo@Jacopos-MacBook-Pro.local>
2021-04-10 20:06:00 +0200

    TailwindCSS 0.2

--- src/components/layout/navbar/navbar.component.jsx
+++ src/components/layout/navbar/navbar.component.jsx
@@ -68,7 +68,7 @@ const Nav = ({ auth: { isAuthenticated }, logout }) => {
   const guestLinks = (
     <>
       <div className="flex items-center justify-center space-x-8 lg:flex">
-        <Link to="/profiles">
+        <Link to="/projects">
           <h1 className="text-lg text-white">All Projects</h1>
           {/* <svg
             xmlns="http://www.w3.org/2000/svg"
