cd865b1bbeb8d330c4149d12a7eb68518e1003d1
Jacopo Costantini <macpapo@Jacopos-MacBook-Pro.local>
2021-04-10 23:50:19 +0200

    TailwindCSS 0.3.1

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const SearchBox = ({ placeholder, handleChange }) => (
   <div class="p-8">
-    <div class="bg-white flex items-center rounded-full shadow-xl m-20">
+    <div class=" bg-gray-100 flex items-center rounded-full shadow-xl mb-20 mr-20 ml-20 mt-5">
       <input
-        class="rounded-l-full w-full py-0 px-6 text-gray-700 leading-tight focus:outline-none"
+        class="rounded-l-full w-full py-0 px-6 bg-gray-100 text-gray-700 leading-tight focus:outline-none text-lg font-mono"
         id="search"
         type="text"
         placeholder="Search"
