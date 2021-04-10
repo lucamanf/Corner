3805ef75dd3fb39ff1cf51394b71ea42fe337793
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-04-10 18:45:01 +0200

    add search button

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -12,7 +12,20 @@ export const SearchBox = ({ placeholder, handleChange }) => (
       />
       <div class="p-4">
         <button class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-            icon
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
         </button>
       </div>
     </div>
