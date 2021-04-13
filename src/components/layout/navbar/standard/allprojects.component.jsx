ba4c0511bb327d9547baa554f4a189520a6b74fe
Jacopo Costantini <macpapo@Jacopos-MacBook-Pro.local>
2021-04-13 17:14:24 +0200

    TailwindCSS 0.3.4.6

--- src/components/layout/navbar/standard/allprojects.component.jsx
+++ src/components/layout/navbar/standard/allprojects.component.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AllProjectsSVG from "../svg/standard/allproject.svg";
 
 export default function AllProjects() {
   return (
     <Link to="/projects">
       <h1 className="flex flex-row items-center justify-center text-lg text-white">
         All Projects
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5 mx-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-          />
-        </svg>
+        <AllProjectsSVG />
       </h1>
     </Link>
   );
--- src/components/layout/navbar/standard/dashboard.component.jsx
+++ src/components/layout/navbar/standard/dashboard.component.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DashboardSVG from "../svg/standard/dashboard.svg";
 
 export default function Dashboard() {
   return (
     <Link to="/dashboard">
       <h1 className="flex flex-row items-center justify-center text-lg text-white">
         Dashboard
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 ml-1.5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
-          />
-        </svg>
+        <DashboardSVG />
       </h1>
     </Link>
   );
