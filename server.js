55c26cabd3f58714daa7e30a47462eb2798f1e39
Matteo Ciriaci <matteo.ciriaci15@gmail.com>
2021-04-16 17:38:23 +0200

    cors x2

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ connection.once('open', () => {
 // Init Middleware
 app.use(express.json());
 app.use(cors());
-res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 
 // Define Routes
