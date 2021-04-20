72b889eb535bd5cb7414f7102e0bd2667d6bbdd4
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-04-20 10:12:29 +0200

    add draft

--- src/components/project-description/project-description.component.jsx
+++ src/components/project-description/project-description.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import RichEditor from "../richTextEditor/richTextEditor.component";
 
 export const ProjectDescription = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="w-full max-w-xl">
         <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+          <RichEditor />
           <div className="mb-4">
             <label
               className="block mb-2 text-sm font-semibold text-gray-600"
