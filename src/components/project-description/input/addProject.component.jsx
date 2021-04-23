3de6eb2ffd4caef36e3ab22ce5e22498e3d5f538
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-04-23 08:38:51 +0200

    POST non funziona

--- src/components/project-description/input/addProject.component.jsx
+++ src/components/project-description/input/addProject.component.jsx
@@ -6,6 +6,22 @@ export const AddProject = () => {
       <button
         className="px-4 py-2 font-bold text-white bg-gray-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
         type="button"
+        onClick={() => (
+          fetch("https://corner-mern.herokuapp.com/api/projects/add"),
+          {
+            method: "POST",
+            headers: { "x-auth-token": localStorage.getItem("token") },
+            body: {
+              titolo: "",
+              materia: "",
+              partecipanti: "",
+              date: "",
+              descrizione: "",
+              contenuto: localStorage.getItem("content"),
+              image_src: "",
+            },
+          }
+        )}
       >
         Aggiungi Progetto
       </button>
--- src/components/textEditor/textEditor.component.jsx
+++ src/components/textEditor/textEditor.component.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState } from "draft-js";
 import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { convertToRaw } from "draft-js";
-// import draftToHtml from "draftjs-to-html";
+import draftToHtml from "draftjs-to-html";
+import AddProject from "../project-description/input/addProject.component";
 
 export default class TextEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
+      content: [],
     };
   }
 
@@ -20,14 +22,20 @@ export default class TextEditor extends Component {
   };
 
   render() {
-    // const hashConfig = {
-    //   trigger: "#",
-    //   separator: " ",
-    // };
-    const { editorState } = this.state;
-    // const rawContentState = convertToRaw(editorState.getCurrentContent());
-
-    // const markup = draftToHtml(rawContentState, hashConfig);
+    const hashConfig = {
+      trigger: "#",
+      separator: " ",
+    };
+    const { editorState, content } = this.state
+    const rawContentState = convertToRaw(editorState.getCurrentContent());
+    const markup = draftToHtml(rawContentState, hashConfig);
+    console.log("MARKUP", markup);
+    console.log("ARRAT", rawContentState);
+    this.state.content = markup;
+    console.log("Contenuto---", content);
+    localStorage.setItem("content", content);
+    console.log("Content in Local Storage", localStorage.getItem("content"));
+    // <AddProject contents={content}/>
     return (
       <Editor
         editorState={editorState}
